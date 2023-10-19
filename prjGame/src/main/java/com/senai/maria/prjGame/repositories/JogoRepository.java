efa30747532a98e6db935cf9d13bacf4157233cc
Maria Eduarda <133990702+Maheduardaocon@users.noreply.github.com>
2023-10-19 13:20:05 -0300

    Delete prjGame directory

--- prjGame/src/main/java/com/senai/maria/prjGame/repositories/JogoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.senai.maria.prjGame.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.senai.maria.prjGame.entities.Jogo;
-
-public interface JogoRepository extends JpaRepository<Jogo, Long>{
-
-}
